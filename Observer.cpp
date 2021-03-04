721ba6622df3034f9cd02c534180aa37710c8c55
Polina <polina.federagina@gmail.om>
2021-03-04 23:26:14 +0700

    Убрана консоль из наблюдателя

--- Observer.cpp
+++ Observer.cpp
@@ -8,19 +8,6 @@ void Observer:: Update(QString a,bool b,qint32 c) {
     if (a==name){
                    exist=b;
                    size=c;
-    std::cout << "New data for observer:"<< std::endl;
-    FileInfo();}
-};
-
-
-void Observer:: FileInfo() {
-      QTextStream cout(stdout), cin(stdin);
-    if (exist) {
-        QTextStream(stdout) << "File name  " << name << endl;
-        std::cout << "File size  " << size << std::endl;
+                   emit NewInfo(this);
     }
-    else {
-        QTextStream(stdout) << "File named " << name <<" does not exist" <<endl;
-    }
-
 };
--- Observer.h
+++ Observer.h
@@ -16,36 +16,51 @@ using namespace std;
 class Observer: public QObject
 {
     Q_OBJECT
-
  public:
 
-  Observer(QString a = " ", bool b = false, qint32 c = 0)
-    {
-        name = a; //имя
-        exist = b; //существует
-        size = c; //размер
-    }
+   Observer(QString a = " ", bool b = false, qint32 c = 0)
+      {
+          name = a; //имя
+          exist = b; //существует
+          size = c; //размер
+          emit requestInformation(name,exist,size);
+      }
 
   virtual ~Observer(){};
-
-  void FileInfo();
 
   void SetName(QString newname){
       name = newname;
       emit requestInformation(name,exist,size);
+
   };
+
+  QString NameInfo(){
+      return name;
+  }
+
+  bool ExistInfo(){
+      return exist;
+  }
+
+  qint32 SizeInfo(){
+      return size;
+  }
+
+
 
 signals:
 
   void requestInformation(QString,bool,qint32);
+  void NewInfo(Observer*);
 
 public slots:
    void Update(QString,bool,qint32);
 
 private:
-  QString name; //имя
-  bool exist;  //существует
-  qint32 size;  //размер
+   QString name; //имя
+   bool exist;  //существует
+   qint32 size;  //размер
+
 };
 
 #endif // OBSERVER_H
